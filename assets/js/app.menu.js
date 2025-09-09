18046503e9eb19f600fd8495177730ccab4081a3
Phanet168 <69040658+Phanet168@users.noreply.github.com>
2025-09-09 16:21:56 +0700

    Update app.menu.js

--- assets/js/app.menu.js
+++ assets/js/app.menu.js
@@ -1,6 +1,6 @@
 import { getAuth, isSuper } from './app.auth.js';
 
-const GAS_BASE = "https://script.google.com/macros/s/AKfycbwuvNrQOG7CoQiEb6LXyz-0KJgir_H5LPjGrcS79Vf9qH-0sU9Mln5N3YvQJn4u_n74HA/exec";
+const GAS_BASE = "https://script.google.com/macros/s/AKfycbwkhRDOYDKb5nqGEhxNKGbHnpoZNGmN4GJlv0-8FTQXNGqJIV1Xfy9XLkXwNfGepC3prQ/exec";
 
 async function gasList(route, params = {}) {
   const u = new URL(GAS_BASE);
@@ -56,4 +56,5 @@ export async function buildDeptMenu(targetUlId = 'deptMenu') {
     box.innerHTML = `<li class="nav-item"><a href="#"><span class="item-name text-danger">បរាជ័យ: ${err.message}</span></a></li>`;
   }
 }
+
 
