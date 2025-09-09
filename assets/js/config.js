af832454c2ce482acc013d602e972a55fe75f644
Phanet168 <69040658+Phanet168@users.noreply.github.com>
2025-09-10 00:38:24 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+// assets/js/config.js
+export const GAS_BASE =
+  "https://script.google.com/macros/s/AKfycbzUXPrbr-q3zTP1Jg-f8PX34Jjj23jHFuYiCyaKuOEmP4Qwzb1pXdFcnbypEZVigdF4IA/exec";
+// ^ ប្រើតែមួយនេះគ្រប់ទំព័រ (ដាក់ URL ដែលអ្នក Deploy ចុងក្រោយ)
