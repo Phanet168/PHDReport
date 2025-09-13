c384116f0662de6dc40c808be2a78b7cc97a1509
Phanet168 <69040658+Phanet168@users.noreply.github.com>
2025-09-13 22:08:24 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // assets/js/config.js
 export const GAS_BASE =
-  "https://script.google.com/macros/s/AKfycby2GC1camn1yO0JEuRgXeyRulKy6iyfcYablx2rKyNWQli02gu99u3L9fO0vROKujlw/exec";
+  "https://script.google.com/macros/s/AKfycbx19YCqe7N2WiX0LpZGVVxw19_m2lAD0EoVAA3fUvae4AldYNJ0Pd7Q06B2pWranss0tQ/exec";
 // ^ ប្រើតែមួយនេះគ្រប់ទំព័រ (ដាក់ URL ដែលអ្នក Deploy ចុងក្រោយ)
